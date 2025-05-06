1f19541cc27ac5083c904a2fcbf21ea972cd0309
Espen Hovlandsdal <espen@hovlandsdal.com>
2025-05-06 10:41:37 -0700

    feat: initial version
    * feat: initial version
    
    * refactor: rename from `doc` to `data`
    
    * refactor: update readme with new `data` property

---
+++ test/definers.test-d.ts
@@ -0,0 +1,56 @@
+import {assertType, describe, expect, expectTypeOf, test} from 'vitest'
+import {
+  documentEventHandler,
+  type DocumentEventHandler,
+  type FunctionContext,
+  type DocumentEvent,
+} from '../src'
+
+describe('documentEventHandler', () => {
+  const context: FunctionContext = {
+    clientOptions: {projectId: 'abc123', dataset: 'xyz789', token: 'sk_some-token'},
+  }
+
+  const event: DocumentEvent = {
+    data: {_id: 'docId', _type: 'docType'},
+  }
+
+  test('has correct type signature', () => {
+    expectTypeOf(documentEventHandler).toBeFunction()
+    expectTypeOf(documentEventHandler).parameter(0).toExtend<DocumentEventHandler>()
+    expectTypeOf(documentEventHandler).returns.toExtend<DocumentEventHandler>()
+
+    // @ts-expect-error should be a function
+    assertType(documentEventHandler('foo'))
+  })
+
+  test('handler envelope has correct types', () => {
+    const handler = documentEventHandler((envelope) => {
+      expectTypeOf(envelope.context).toEqualTypeOf<FunctionContext>()
+      expectTypeOf(envelope.event).toEqualTypeOf<DocumentEvent>()
+      expect(envelope.context).toEqual(context)
+      expect(envelope.event).toEqual(event)
+      expectTypeOf(envelope.event.data).toBeAny()
+    })
+
+    handler({context, event})
+  })
+
+  test('can pass data type as generic', () => {
+    const handler = documentEventHandler<{foo: string}>((envelope) => {
+      expectTypeOf(envelope.event.data).toEqualTypeOf<{foo: string}>()
+      expect(envelope.event.data.foo).toBe('bar')
+    })
+
+    handler({context, event: {data: {foo: 'bar'}}})
+
+    const unknownHandler = documentEventHandler<unknown>((envelope) => {
+      expectTypeOf(envelope.event.data).toEqualTypeOf<unknown>()
+
+      // @ts-expect-error accessing `foo` on unknown should fail
+      expect(envelope.event.data.foo).toBe('bar')
+    })
+
+    unknownHandler({context, event})
+  })
+})
