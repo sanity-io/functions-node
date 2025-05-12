ce74076c9888d39dcef35d1892c4d2d1eae9a90e
Simon MacDonald <simon.macdonald@gmail.com>
2025-05-12 14:10:20 -0400

    feat: RUN-503: Rename documentEventHandler to defineDocumentEventHandler
    Signed-off-by: Simon MacDonald <simon.macdonald@gmail.com>

--- src/definers.ts
+++ src/definers.ts
@@ -1,6 +1,7 @@
 import type {DocumentEventHandler} from './types.js'
 
 /**
+ * @deprecated since version 1.0.3
  * Defines a "document event" function handler.
  * Returns the handler function as-is, only providing the types and doing basic validation.
  *
@@ -8,6 +9,19 @@ import type {DocumentEventHandler} from './types.js'
  * @returns The handler function, unmodified.
  */
 export function documentEventHandler<IData = any>(
+  handler: DocumentEventHandler<IData>,
+): DocumentEventHandler<IData> {
+  return defineDocumentEventHandler(handler)
+}
+
+/**
+ * Defines a "document event" function handler.
+ * Returns the handler function as-is, only providing the types and doing basic validation.
+ *
+ * @param handler - The event handler function to use.
+ * @returns The handler function, unmodified.
+ */
+export function defineDocumentEventHandler<IData = any>(
   handler: DocumentEventHandler<IData>,
 ): DocumentEventHandler<IData> {
   if (typeof handler !== 'function') throw new TypeError('`handler` must be a function')
