39589fe3eaf30d76945a537279bc4ab80064c380
filmaj <fil.maj@sanity.io>
2026-04-10 17:27:43 -0400

    docs: adding automatic reference doc generation via typedoc

--- src/definers.ts
+++ src/definers.ts
@@ -16,7 +16,8 @@ export function documentEventHandler<IData = any>(handler: DocumentEventHandler<
  * Defines a "scheduled event" function handler.
  * Returns the handler function as-is, only providing the types and doing basic validation.
  *
- * @beta
+ * @alpha
+ * @hidden
  * @param handler - The event handler function to use.
  * @returns The handler function, unmodified.
  */
