53486d862015f25f99ff98af13a922477884a9a0
Stephen Fisher <stephen.fisher@sanity.io>
2026-08-11 15:18:42 -0600

    jsdoc

--- src/types.ts
+++ src/types.ts
@@ -302,7 +302,11 @@ export interface DurableWaitForConditionOptions<T> {
   next: (state: T, context: DurableContext) => DurableWaitForConditionDecision
 }
 
-// @todo: follow-up to better define separated context
+/**
+ * // @todo: follow-up to better define separated context
+ * @alpha Using durables is considered experimental and may change in the future.
+ * @hidden
+ */
 export type DurableOperationArgs = {
   ctx: DurableContext
   logger?: DurableLogger
